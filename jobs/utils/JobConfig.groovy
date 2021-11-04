b8bec74a76936255649264b0146793e89d835032
Antonio Sostar <antonio.sostar56@gmail.com>
2021-11-04 08:49:33 +0100

    wip

--- jobs/utils/JobConfig.groovy
+++ jobs/utils/JobConfig.groovy
@@ -32,46 +32,16 @@ class JobConfig {
     static def basicPipeline(job, repo, includeBranches = "master *.x-maintenance PR-*", ignoreOnPush = false,
                              buildPR = true, jenkinsfilePath = "Jenkinsfile", cronTrigger = 'H 23 * * *') {
         job.with {
-            configure {
-                it / sources(class: 'jenkins.branch.MultiBranchProject$BranchSourceList') / 'data' / "jenkins.branch.BranchSource" {
-                    source(class: "org.jenkinsci.plugins.github_branch_source.GitHubSCMSource") {
-                        id('GitHubSource')
-                        repoOwner('tonac')
-                        repository(repo)
-                        ignoreOnPushNotifications(true)
-
-                        traits {
-                            'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
-                                strategyId('1')
-                            }
-                            if (buildPR) {
-                                'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
-                                    strategyId('1')
-                                }
-                            }
-                            'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
-                                includes(includeBranches)
-                                excludes()
-                            }
-                            if (ignoreOnPush) {
-                                'jenkins.plugins.git.traits.IgnoreOnPushNotificationTrait'()
+            definition {
+                cpsScm {
+                    scm {
+                        git {
+                            remote {
+                                github("tonac/$repo")
                             }
                         }
                     }
-                    strategy(class: "jenkins.branch.DefaultBranchPropertyStrategy") {
-                        properties(class: "empty-list")
-                    }
                 }
-            }
-
-            orphanedItemStrategy {
-                discardOldItems {
-                    daysToKeep(1)
-                }
-            }
-
-            triggers {
-                cron(cronTrigger)
             }
 
         }
