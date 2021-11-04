7998d99c3a9f354191b5e4d1bb4dfdac8a8a8485
Antonio Sostar <antonio.sostar56@gmail.com>
2021-11-04 07:52:42 +0100

    fix

--- jobs/spring_boot_gradle_boilerplate_jobs.groovy
+++ jobs/spring_boot_gradle_boilerplate_jobs.groovy
@@ -1,5 +1,6 @@
 import utils.JobConfig
 
+folder('Demo')
 def springBootPipelineJob = multibranchPipelineJob('Demo/Spring Boot App Multibranch Pipeline')
 JobConfig.basicPipeline(
         springBootPipelineJob,
