6cf31ed686eed357794418d58b177e2c35ed43dc
Antonio Sostar <antonio.sostar56@gmail.com>
2021-11-04 07:43:37 +0100

    fix

--- jobs/utils/JobConfig.groovy
+++ jobs/utils/JobConfig.groovy
@@ -1,70 +1,73 @@
-static
-def basicPipeline(job, repo, includeBranches = "master *.x-maintenance PR-*", ignoreOnPush = false,
-                  buildPR = true, jenkinsfilePath = "Jenkinsfile", interval = 1440) {
+package utils
 
-    job.with {
-        configure {
-            it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
-                owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
-                scriptPath(jenkinsfilePath)
-            }
+class JobConfig {
 
-            it / sources(class: 'jenkins.branch.MultiBranchProject$BranchSourceList') / 'data' / "jenkins.branch.BranchSource" {
-                source(class: "org.jenkinsci.plugins.github_branch_source.GitHubSCMSource") {
-                    id('GitHubSource')
-                    repoOwner('tonac')
-                    repository(repo)
-                    ignoreOnPushNotifications(true)
+    static def basicPipeline(job, repo, includeBranches = "master *.x-maintenance PR-*", ignoreOnPush = false,
+                             buildPR = true, jenkinsfilePath = "Jenkinsfile", interval = 1440) {
 
-                    traits {
-                        'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
-                            strategyId('1')
-                        }
-                        if (buildPR) {
-                            'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+        job.with {
+            configure {
+                it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
+                    owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
+                    scriptPath(jenkinsfilePath)
+                }
+
+                it / sources(class: 'jenkins.branch.MultiBranchProject$BranchSourceList') / 'data' / "jenkins.branch.BranchSource" {
+                    source(class: "org.jenkinsci.plugins.github_branch_source.GitHubSCMSource") {
+                        id('GitHubSource')
+                        repoOwner('tonac')
+                        repository(repo)
+                        ignoreOnPushNotifications(true)
+
+                        traits {
+                            'org.jenkinsci.plugins.github__branch__source.BranchDiscoveryTrait' {
                                 strategyId('1')
                             }
-                        }
-                        'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
-                            includes(includeBranches)
-                            excludes()
-                        }
-                        'org.jenkinsci.plugins.github__branch__source.SSHCheckoutTrait' {
-                            credentialsId(JENKINS_CREDENTIALS_FOR_GITHUB)
-                        }
-                        'jenkins.plugins.git.traits.CloneOptionTrait' {
-                            extension(class: "hudson.plugins.git.extensions.impl.CloneOption") {
-                                shallow("false")
-                                noTags("false")
-                                depth("0")
-                                honorRefspec("false")
+                            if (buildPR) {
+                                'org.jenkinsci.plugins.github__branch__source.OriginPullRequestDiscoveryTrait' {
+                                    strategyId('1')
+                                }
+                            }
+                            'jenkins.scm.impl.trait.WildcardSCMHeadFilterTrait' {
+                                includes(includeBranches)
+                                excludes()
+                            }
+                            'org.jenkinsci.plugins.github__branch__source.SSHCheckoutTrait' {
+                                credentialsId(JENKINS_CREDENTIALS_FOR_GITHUB)
+                            }
+                            'jenkins.plugins.git.traits.CloneOptionTrait' {
+                                extension(class: "hudson.plugins.git.extensions.impl.CloneOption") {
+                                    shallow("false")
+                                    noTags("false")
+                                    depth("0")
+                                    honorRefspec("false")
+                                }
+                            }
+                            if (ignoreOnPush) {
+                                'jenkins.plugins.git.traits.IgnoreOnPushNotificationTrait'()
                             }
                         }
-                        if (ignoreOnPush) {
-                            'jenkins.plugins.git.traits.IgnoreOnPushNotificationTrait'()
-                        }
+                    }
+                    strategy(class: "jenkins.branch.DefaultBranchPropertyStrategy") {
+                        properties(class: "empty-list")
                     }
                 }
-                strategy(class: "jenkins.branch.DefaultBranchPropertyStrategy") {
-                    properties(class: "empty-list")
+            }
+
+            orphanedItemStrategy {
+                discardOldItems {
+                    daysToKeep(1)
                 }
             }
-        }
 
-        orphanedItemStrategy {
-            discardOldItems {
-                daysToKeep(1)
+            triggers {
+                if (interval > 0) {
+                    periodic(interval)
+                } else {
+                    cron('H 23 * * *')
+                }
             }
-        }
 
-        triggers {
-            if (interval > 0) {
-                periodic(interval)
-            } else {
-                cron('H 23 * * *')
-            }
         }
-
     }
 }
-// Fix for No Exact Tip revision issue on jenkins
